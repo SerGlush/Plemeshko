4c0ebc658ba92cd626d03210421e613c26da3239
SerGlush <Shoka583@gmail.com>
2023-01-12 17:24:58 +0300

    add time option to Setting

--- plemeshko/src/server/config/setting_group.rs
+++ plemeshko/src/server/config/setting_group.rs
@@ -4,11 +4,14 @@ use plegine::{
 };
 use plegine_derive::{Config, FromValue};
 
+use crate::server::units::Ticks;
+
 use super::resource::signed_storage::ResourceStorageSigned;
 
 #[derive(FromValue)]
 pub struct Setting {
     pub resources: Vec<ResourceStorageSigned>,
+    pub time_to_complete: Option<Ticks>,
 }
 
 #[derive(Config)]
